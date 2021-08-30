7f96df1d4044d0b673e13889353d5317c86af877
zaaack <zaaack@qq.com>
2021-08-30 14:46:10 +0800

    feat: input text

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,52 @@
 import net from 'net'
 import robot from 'robotjs'
 import Express from 'express'
-import bodyParser from 'express'
 import Enquirer from 'enquirer'
 import path from 'path'
-const {
-  Select
-} = require('enquirer')
+const { Select } = require('enquirer')
 
 const app = Express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-app.use(bodyParser.raw())
+app.use(Express.json())
+app.use(
+  Express.urlencoded({
+    extended: true,
+  }),
+)
+app.use(Express.raw())
 
-app
-  .get('/mouse/get/', (req, res) => {
-    res.json(robot.getMousePos())
+app.get('/mouse/get/', (req, res) => {
+  res.json(robot.getMousePos())
+})
+app.post('/mouse/move', (req, res) => {
+  const { x, y } = req.body
+  robot.moveMouse(x, y)
+  res.json({
+    x,
+    y,
   })
-  .post('/mouse/move', (req, res) => {
-    const {
-      x,
-      y
-    } = req.body
-    robot.moveMouse(x, y)
-    res.json({
-      x,
-      y
-    })
+})
+app.post('/mouse/click', (req, res) => {
+  const { button } = req.body
+  robot.mouseClick(button)
+  res.json({
+    button,
   })
-  .post('/mouse/click', (req, res) => {
-    const {
-      button
-    } = req.body
-    robot.mouseClick(button)
-    res.json({
-      button
-    })
-  })
-  .post('/mouse/scroll', (req, res) => {
-    const {
-      x,
-      y
-    } = req.body
-    robot.scrollMouse(x, y)
-    res.json(req.body)
-  })
-  app.use(Express.static(path.join(__dirname, '../public')))
+})
+app.post('/mouse/scroll', (req, res) => {
+  const { x, y } = req.body
+  robot.scrollMouse(x, y)
+  res.json(req.body)
+})
+
+
+app.post('/keyboard/input', (req, res) => {
+  const { value } = req.body
+  robot.typeString(value)
+  res.json(req.body)
+})
+
+app.use(Express.static(path.join(__dirname, '../public')))
 
 function getExternHosts() {
   const hosts = []
@@ -57,8 +55,7 @@ function getExternHosts() {
     const iface = interfaces[devName]
     for (let i = 0; i < iface.length; i++) {
       const alias = iface[i]
-      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias
-        .internal) {
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
         hosts.push(alias.address)
       }
     }
@@ -69,38 +66,37 @@ function getExternHosts() {
 async function start() {
   let port = await findPort()
   let server = app.listen(port, () => {
-    const address = server.address() !
-      if (typeof address === 'string') {
-        console.log(`Listening on ${address}`)
-      } else {
-        const hosts = getExternHosts().map((host) =>
-          `http://${host}:${address.port}`)
+    const address = server.address()!
+    if (typeof address === 'string') {
+      console.log(`Listening on ${address}`)
+    } else {
+      const hosts = getExternHosts().map((host) => `http://${host}:${address.port}`)
 
-        const select = (initial: number) => {
-          const prompt = new Select({
-            name: 'select',
-            message: 'Select external url:',
-            choices: hosts.slice(),
-            initial,
-          })
+      const select = (initial: number) => {
+        const prompt = new Select({
+          name: 'select',
+          message: 'Select external url:',
+          choices: hosts.slice(),
+          initial,
+        })
 
-          prompt
-            .run()
-            .then((answer: string) => {
-              select(hosts.indexOf(answer))
-            })
-            .catch(console.error)
-          prompt.on('cancel', () => {
-            process.exit()
+        prompt
+          .run()
+          .then((answer: string) => {
+            select(hosts.indexOf(answer))
           })
-          console.log(hosts[initial])
-          require('qrcode-terminal').generate(hosts[initial], {
-            small: true
-          })
-        }
-
-        select(0)
+          .catch(console.error)
+        prompt.on('cancel', () => {
+          process.exit()
+        })
+        console.log(hosts[initial])
+        require('qrcode-terminal').generate(hosts[initial], {
+          small: true,
+        })
       }
+
+      select(0)
+    }
   })
 }
 
@@ -119,6 +115,5 @@ async function findPort(port = 8610) {
       })
   })
 }
-
 
 start().catch(console.error)
